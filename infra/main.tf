954fed9cce631351ec16a1659b3977fd6ddfa69c
Timo Lehtonen <timo.lehtonen@solita.fi>
2023-05-17 14:54:27 +0300

    feat(infra): add PostgreSQL server, database, and firewall rule resources to the Terraform configuration
    Add a PostgreSQL server, database, and firewall rule resources to the Terraform configuration to enable the application to store data in a PostgreSQL database. The server is created with the name "lynx-order-tracker-dev-db", located in the same resource group as the other resources, and with a SKU of "B_Gen5_1". The database is created with the same name as the server, and with a charset of "UTF8" and a collation of "English_United States.1252". A random password is generated for the PostgreSQL server's administrator login. A firewall rule is created to allow connections from the IP address "95.175.96.152".

--- infra/main.tf
+++ infra/main.tf
@@ -49,3 +49,44 @@ resource "azurerm_resource_group" "resource_group" {
     DueDate = "2023-05-31"
   }
 }
+
+# PostgreSQL db
+resource "azurerm_postgresql_server" "postgresql_server" {
+  name                = "lynx-order-tracker-dev-db"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  sku_name            = "B_Gen5_1"
+  storage_mb          = 5120
+  version             = "11"
+  ssl_enforcement_enabled = true
+
+  administrator_login          = "lynx-order-tracker-dev"
+  administrator_login_password = random_password.postgresql_password.result
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_postgresql_database" "postgresql_database" {
+  name                = "lynx-order-tracker-dev-db"
+  resource_group_name = azurerm_resource_group.resource_group.name
+  server_name         = azurerm_postgresql_server.postgresql_server.name
+  charset             = "UTF8"
+  collation           = "English_United States.1252"
+}
+
+resource "random_password" "postgresql_password" {
+  length           = 16
+  special          = true
+  override_special = "_%@"
+}
+
+# PostgreSQL firewall rule
+resource "azurerm_postgresql_firewall_rule" "postgresql_firewall_rule" {
+  name                = "AllowAllWindowsAzureIps"
+  resource_group_name = azurerm_resource_group.resource_group.name
+  server_name         = azurerm_postgresql_server.postgresql_server.name
+  start_ip_address    = "95.175.96.152"
+  end_ip_address      = "95.175.96.152"
+}
